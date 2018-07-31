ca6cfa4adf06244044656ef5cd863e653f3275bc
Ayman Mobin <aymamob@gmail.com>
2018-07-31 09:14:39 -0400

    data types

---
+++ DataTypes/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    float num1 = 27.333f;
+    double num2 = 54.23e+11;
+    _Bool boolVar = 1;
+    long deficit = 1.234+7L;
+    unsigned int num3 = 44;
+    return 0;
+}
