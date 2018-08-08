8888496ea9a53589fb506ab6e3f5b7834bd76c7b
Ayman Mobin <aymamob@gmail.com>
2018-08-08 00:23:28 -0400

    added functions

--- Functions/main.c
+++ Functions/main.c
@@ -2,13 +2,36 @@
 #include <stdlib.h>
 
 void add();
+void subtract();
+void multiply();
+void divide();
 
 int main()
 {
-    add();
+    add(4, 5);
+    subtract(6, 1);
+    multiply(2, 3);
+    divide(9, 3);
     return 0;
 }
 
-void add(){
-
+void add(int num1, int num2){
+    int result = num1 + num2;
+    printf("%d\n", result);
 }
+
+void subtract(int num1, int num2){
+    int result = num1 - num2;
+    printf("%d\n", result);
+}
+
+void multiply(int num1, int num2){
+    int result = num1 * num2;
+    printf("%d\n", result);
+}
+void divide(int num1, int num2){
+    int result = num1 / num2;
+    printf("%d\n", result);
+}
+
+
